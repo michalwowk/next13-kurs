c14aa17edfb52dcd6bcbc66dbe496d2f23641c1f
Michał Wowk <michalwowk92@gmail.com>
2023-09-08 21:18:54 +0200

    fix: change h2 to h3, fix e2e test

--- src/app/ui/atoms/ProductListItemDescription.tsx
+++ src/app/ui/atoms/ProductListItemDescription.tsx
@@ -11,7 +11,7 @@ export const ProductListItemDescription = ({
 	return (
 		<div className="mt-2 flex justify-between">
 			<div>
-				<h2 className="text-sm font-semibold text-gray-700">{name}</h2>
+				<h3 className="text-sm font-semibold text-gray-700">{name}</h3>
 				<p className="text-sm font-medium text-gray-500">
 					<span className="sr-only">Kategoria:</span> {category}
 				</p>
