9652fc691378f499a6accd44de9aa33ca5d9622e
Michał Wowk <michalwowk92@gmail.com>
2023-09-13 12:40:52 +0200

    feat: add basic static pages

---
+++ src/app/(static-pages)/polityka-prywatnosci/page.tsx
@@ -0,0 +1,8 @@
+export default function Policy() {
+	return (
+		<>
+			<h1 className="text-4xl font-bold">Polityka prywatności</h1>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, maxime?</p>
+		</>
+	);
+}
---
+++ src/app/(static-pages)/regulamin/page.tsx
@@ -0,0 +1,8 @@
+export default function TermsAndConditions() {
+	return (
+		<>
+			<h1 className="text-4xl font-bold">Regulamin</h1>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, maxime?</p>
+		</>
+	);
+}
