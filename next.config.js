691552cfe5579419edfb171083bb939c0dfcef0d
Michał Wowk <michalwowk92@gmail.com>
2023-09-16 11:22:14 +0200

    feat: handle redirect from /products to /produts/1 page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
 	experimental: {
 		typedRoutes: true,
 	},
+	async redirects() {
+		return [
+			{
+				source: "/products",
+				destination: "/products/1",
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
