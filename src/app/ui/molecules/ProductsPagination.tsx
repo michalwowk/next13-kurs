df5222b3d76dad1e0004d752d410a439689b1033
Michał Wowk <michalwowk92@gmail.com>
2023-10-10 20:17:30 +0200

    fix: pagination tests

--- src/app/ui/molecules/ProductsPagination.tsx
+++ src/app/ui/molecules/ProductsPagination.tsx
@@ -11,10 +11,10 @@ export const ProductsPagination = ({ totalNumberOfPages, route }: ProductsPagina
 		return null;
 	}
 	return (
-		<div data-testid="products-list" className="center mt-10 flex justify-center gap-2">
+		<ul aria-label="pagination" className="center mt-10 flex justify-center gap-2">
 			{Array.from({ length: totalNumberOfPages }, (_, i) => i).map((pageNumber) => {
 				return (
-					<div key={pageNumber}>
+					<li key={pageNumber}>
 						<ActiveLink
 							className="bg-slate-100 px-3 py-2 hover:bg-slate-300"
 							href={`${route}/${pageNumber + 1}` as Route}
@@ -22,9 +22,9 @@ export const ProductsPagination = ({ totalNumberOfPages, route }: ProductsPagina
 						>
 							{pageNumber + 1}
 						</ActiveLink>
-					</div>
+					</li>
 				);
 			})}
-		</div>
+		</ul>
 	);
 };
