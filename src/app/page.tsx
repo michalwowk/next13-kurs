2cce7ef268e5d1d8befb1a39593ec2572e433886
Michał Wowk <michalwowk92@gmail.com>
2023-10-10 22:03:58 +0200

    fix: e2e tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { notFound } from "next/navigation";
+import { getProductsList } from "@/api/products";
+import { ProductList } from "@/ui/organisms/ProductList";
+
+export default async function Home() {
+	const { products } = await getProductsList({
+		first: 8,
+		skip: 0,
+	});
+
+	if (!products.length) {
+		return notFound();
+	}
+
 	return (
 		<main className="container mx-auto">
-			<h1>Hello My Friend!</h1>
+			<ProductList products={products} />
 		</main>
 	);
 }
