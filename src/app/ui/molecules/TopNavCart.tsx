642ac9a9106e28d99611bf54f790d3b795ebedcf
Michał Wowk <michalwowk92@gmail.com>
2023-10-11 23:13:48 +0200

    test

--- src/app/ui/molecules/TopNavCart.tsx
+++ src/app/ui/molecules/TopNavCart.tsx
@@ -11,7 +11,7 @@ export const TopNavCart = async () => {
 	const quantity = cart?.order?.orderItems.reduce((acc, item) => acc + item.quantity, 0) ?? 0;
 
 	return (
-		<Link className="relative flex" href="/cart">
+		<Link className="relative flex" href="/cart" data-testid="cart-icon">
 			<ShoppingCart width={30} height={30} />{" "}
 			<span className="opacity-85 absolute left-5 top-5 flex h-4 w-4 items-center justify-center rounded-full bg-blue-600 text-xs text-slate-100">
 				{quantity}
